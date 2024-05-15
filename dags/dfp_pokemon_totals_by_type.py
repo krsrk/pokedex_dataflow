e67f891caa4eefd1df5a448832a0a7ee403983e4
Chris Lopez <christian.lopez.be@gmail.com>
2024-05-15 12:10:33 -0600

    fix: code of the df. Separate the resul set into vars.

--- dags/dfp_pokemon_totals_by_type.py
+++ dags/dfp_pokemon_totals_by_type.py
@@ -6,8 +6,8 @@ pipeline_options = PipelineOptions(
     runner='DataflowRunner',
     project='proud-limiter-422923-k4',
     region='us-central1',
-    staging_location='gs://pokedex-dataflow/dev/staging',
-    temp_location='gs://pokedex-dataflow/dev/temp'
+    staging_location='gs://us-central1-cmp-sandbox-dev-e1950a8d-bucket/staging',
+    temp_location='gs:/us-central1-cmp-sandbox-dev-e1950a8d-bucket/temp'
 )
 
 
@@ -15,10 +15,10 @@ def run_pipeline():
     with beam.Pipeline(options=pipeline_options) as pipeline:
         poke_data = (
                 pipeline
-                | 'ReadFromGCS' >> beam.io.ReadFromText('gs://pokedex-dataflow/dev/source/pokemon.csv')
+                | 'ReadFromGCS' >> beam.io.ReadFromText('gs://us-central1-cmp-sandbox-dev-e1950a8d-bucket/data/pokemon.csv')
         )
 
-        result_set = (
+        '''result_set = (
                 poke_data
                 | "Filter by Pokemon Type Name" >> beam.Filter(
                                                         lambda pokemon: 'type' in pokemon and 'type_name' in pokemon
@@ -28,9 +28,33 @@ def run_pipeline():
                 | "Formatted Data" >> beam.Map(
                                             lambda tipo_conteo: f"type_name: {tipo_conteo[1][0]}, total: {str(len(tipo_conteo[1]))}"
                                       )
+        )'''
+
+        filter_data = (
+                poke_data
+                | "FilterPokemonTypeName" >> beam.Filter(lambda pokemon: 'type' in pokemon and 'type_name' in pokemon)
         )
 
-        result_set | 'WriteToBigQuery' >> beam.io.WriteToBigQuery(
+        extracted_data = (
+                filter_data
+                | "ExtractPokemonType" >> beam.Map(lambda pokemon: (pokemon['type'], pokemon['type_name']))
+        )
+
+        group_data = (
+                extracted_data
+                | "GroupByPokemonType" >> beam.CombinePerKey(beam.combiners.ToListCombineFn())
+        )
+
+        formated_data = (
+                group_data
+                | "Formatted Data" >> beam.Map(
+            lambda tipo_conteo: f"type_name: {tipo_conteo[1][0]}, total: {str(len(tipo_conteo[1]))}")
+        )
+
+        formated_data | "PrintFormattedData" >> beam.Map(print)
+        print(formated_data)
+
+        formated_data | 'WriteToBigQuery' >> beam.io.WriteToBigQuery(
             table='proud-limiter-422923-k4.pokemons.totals_by_type',
             write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
         )
